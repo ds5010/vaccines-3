23ab72558538969f681cfebf1e7b3135e5146c92
Mitchell <hornsby.m@northeastern.edu>
2023-02-17 21:37:43 -0500

    cleaned up code and updated chart

--- src/comparison.py
+++ src/comparison.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import datetime
 from collections import defaultdict
 import sys
 
@@ -14,60 +17,41 @@ def fips_to_name(FIPS):
     return map[FIPS]
 
 def comparison(FIPS_1='44003', FIPS_2='01125'):
-    months = {
-        'May':'05-31-2021',
-        'June':'06-30-2021',
-        'July':'07-31-2021',
-        'August':'08-31-2021',
-        'September':'09-30-2021',
-        'October':'10-31-2021',
-        'November':'11-30-2021'
-        }
+    df_m = pd.read_csv("months.csv")
+    df_m['date2']=df_m['date'].apply(lambda x: datetime.datetime.strptime(x, "%m-%d-%Y"))
+    df_m = df_m.iloc[1:]
+    df_m["month"]=df_m["date2"].apply(lambda x: x.strftime('%m'))
+    df_m.set_index('date', inplace = True)
+    df_m.drop(columns=['date2'], inplace = True)
+
     file_list = []
-    for month, csv_date in months.items():
+    for csv_date, month in df_m.iterrows():
         df = pd.read_csv('data/Merge/vaccinations-and-deaths-' + csv_date +'.csv', converters={'FIPS' : str})
         df['Deaths_Per_1e5'] = df['Deaths'] / df['Census2019_18PlusPop'] * 1e5
-        df['Month'] = month
-        file_list.append(df)
+        df['Month'] = csv_date
+        file_list.append(df)    
+
     merged_df = pd.concat(file_list)
+    print(merged_df.head())
     cleaned_df = merged_df[merged_df['FIPS'].str.contains(FIPS_1+"|"+FIPS_2)]
+    print(cleaned_df.head())
 
-    FIPS_1_name = fips_to_name(FIPS_1)
-    FIPS_2_name = fips_to_name(FIPS_2)
+    ax1 = sns.barplot(data=cleaned_df, x="Month", y="Series_Complete_18PlusPop_Pct", hue="Recip_County")
+    ax2 = sns.lineplot(data=cleaned_df, axes=ax1.twinx(), x="Month", y="Deaths_Per_1e5", hue="Recip_County")
 
-    print("Comparing "+FIPS_1_name+" and "+FIPS_2_name)
-    FIPS_1_vax = cleaned_df.loc[cleaned_df['FIPS']==FIPS_1]
-    FIPS_1_vax = FIPS_1_vax["Series_Complete_18PlusPop_Pct"]
-    FIPS_1_death = cleaned_df.loc[cleaned_df['FIPS']==FIPS_1]
-    FIPS_1_death = FIPS_1_death['Deaths_Per_1e5']
+    handles1, labels1 = ax1.get_legend_handles_labels()
+    handles2, labels2 = ax2.get_legend_handles_labels()
+    labels = [('% vaccinated -- ' if i <2 else 'deaths/100k -- ') + fips_to_name(label) for i, label in enumerate(labels1 + labels2)]
+    ax1.legend(handles1 + handles2, labels)
+    ax2.get_legend().remove()
+
+    blank = mpl.text.Text(0, 0, '')
+    xticklabels = [label if (i % 5) == 0 else blank for i, label in enumerate(ax1.get_xticklabels())]
+    ax1.set_xticklabels(xticklabels)
+
+    plt.savefig("img/comparison.png")
+
     
-    FIPS_2_vax = cleaned_df.loc[cleaned_df['FIPS']==FIPS_2]
-    FIPS_2_vax = FIPS_2_vax["Series_Complete_18PlusPop_Pct"]
-    FIPS_2_death = cleaned_df.loc[cleaned_df['FIPS']==FIPS_2]
-    FIPS_2_death = FIPS_2_death['Deaths_Per_1e5']
-
-    fig,ax1 = plt.subplots() 
-    x = [0, 1, 2, 3, 4, 5, 6]
-    labels = ['May', 'June', 'July', 'August', 'September', 'October', 'November']
-    x = np.arange(len(labels))
-    width = 0.35 
-    ax1.set_xticks(x, labels)
-    ax1.set_ylim(0, 100)
-    ax1.set_xlim(0,6)
-    ax1.set_title('18+ Vaccination Rates and Deaths in '+FIPS_1_name+" and "+FIPS_2_name, wrap=True)
-    ax1.set_xlabel('Month 2021')
-    ax1.set_ylabel('Percent Vaccinated') 
-    ax1.bar(x - width/2, FIPS_1_vax, width, color='darkcyan', label = '% Vaccinated in '+FIPS_1_name)
-    ax1.bar(x + width/2, FIPS_2_vax, width, color='indianred', label = '% Vaccinated in '+FIPS_2_name)
-    ax2 = ax1.twinx()
-    ax2.plot(x, FIPS_1_death, color='darkslategray', label = '# of Deaths in '+FIPS_1_name)
-    ax2.plot(x, FIPS_2_death, color='firebrick', label = '# of Deaths in '+FIPS_2_name)
-    ax2.set_ylabel('Deaths per 100k Population.')
-    handles_1, _ = ax1.get_legend_handles_labels()
-    handles_2, _ = ax2.get_legend_handles_labels()
-    handles_2.extend(handles_1) # adds the list items from handles_1 to handles_2
-    ax2.legend(handles = handles_2, loc='upper left')
-    fig.savefig("img/comparison.png")
 
 def main(argv):
     comparison(argv[0], argv[1])
